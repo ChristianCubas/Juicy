5a4e1a7799ff9b0e4cb22fdfb8ba0104eefebf90
Christian Cubas Jaramillo <christiancubasjaramillo@gmail.com>
2025-09-29 11:33:11 -0500

    Initial commit
    # Conflicts:
    #	.gitignore
    #	.idea/deploymentTargetSelector.xml
    #	.idea/misc.xml
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/juicy/FirstFragment.java
    #	app/src/main/java/com/example/juicy/MainActivity.java
    #	app/src/main/java/com/example/juicy/SecondFragment.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/content_main.xml
    #	app/src/main/res/layout/fragment_first.xml
    #	app/src/main/res/layout/fragment_second.xml
    #	app/src/main/res/menu/menu_main.xml
    #	app/src/main/res/navigation/nav_graph.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/dimens.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/xml/data_extraction_rules.xml
    #	gradle/libs.versions.toml

---
+++ app/src/main/java/com/example/juicy/CrearCuenta.java
@@ -0,0 +1,33 @@
+package com.example.juicy;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.juicy.databinding.FragmentCrearCuentaBinding;
+
+public class CrearCuenta extends Fragment {
+    public FragmentCrearCuentaBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding.btnBack.setOnClickListener(v->{
+            /*FragmentManager manager = getActivity().getSupportFragmentManager();
+            CrearCuenta fragment_CrearCuenta = new CrearCuenta();
+            manager.beginTransaction()
+                    .replace(R.id.nav_host_fragment_content_main,fragment_CrearCuenta)
+                    .addToBackStack(null)
+                    .commit();*/
+        });
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_crear_cuenta, container, false);
+    }
+}
