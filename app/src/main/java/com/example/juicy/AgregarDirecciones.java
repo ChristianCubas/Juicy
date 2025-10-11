6810cfc18c6f7a7ed7bb6da0f3491d10d7223a7c
ronald <ronaldfabian31@gmail.com>
2025-10-11 08:05:26 -0500

    r1

--- app/src/main/java/com/example/juicy/AgregarDirecciones.java
+++ app/src/main/java/com/example/juicy/AgregarDirecciones.java
@@ -3,62 +3,63 @@ package com.example.juicy;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AgregarDirecciones#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AgregarDirecciones extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AgregarDirecciones() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AgregarDirecciones.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AgregarDirecciones newInstance(String param1, String param2) {
-        AgregarDirecciones fragment = new AgregarDirecciones();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText etCategoria, etTipoVia, etNombreVia, etNumeroVia, etDepartamento, etReferencia, etCiudad;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar_direcciones, container, false);
+        View view = inflater.inflate(R.layout.fragment_agregar_direcciones, container, false);
+
+        etCategoria = view.findViewById(R.id.etCategoria);
+        etTipoVia = view.findViewById(R.id.etTipoVia);
+        etNombreVia = view.findViewById(R.id.etNombreVia);
+        etNumeroVia = view.findViewById(R.id.etNumeroVia);
+        etDepartamento = view.findViewById(R.id.etDepartamento);
+        etReferencia = view.findViewById(R.id.etReferencia);
+        etCiudad = view.findViewById(R.id.etCiudad);
+        View btnGuardar = view.findViewById(R.id.btnGuardarDireccion);
+        View btnAbrirMapa = view.findViewById(R.id.btnAbrirMapa);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "Dirección guardada exitosamente", Toast.LENGTH_SHORT).show();
+                limpiarCampos();
+            }
+        });
+
+        btnAbrirMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "Abriendo mapa para seleccionar dirección", Toast.LENGTH_SHORT).show();
+                limpiarCampos();
+                NavHostFragment.findNavController(AgregarDirecciones.this)
+                        .navigate(R.id.buscarMapa);
+            }
+        });
+
+        return view;
+    }
+
+    private void limpiarCampos() {
+        etCategoria.setText("");
+        etTipoVia.setText("");
+        etNombreVia.setText("");
+        etNumeroVia.setText("");
+        etDepartamento.setText("");
+        etReferencia.setText("");
+        etCiudad.setText("");
     }
 }
