cce874385015396308e0aab296735eca758e2937
cesargarcia19 <cesarandregg19@gmail.com>
2025-10-24 14:39:17 -0500

    Actualización de interfaz y mejoras

---
+++ app/src/main/java/com/example/juicy/model/RegistrarClienteRequest.java
@@ -0,0 +1,39 @@
+package com.example.juicy.model;
+
+public class RegistrarClienteRequest {
+    private String email;
+    private String password;
+    private String nro_dni;
+    private String nombre;
+    private String ape_paterno;
+    private String ape_materno;
+    private String celular;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNro_dni(String nro_dni) {
+        this.nro_dni = nro_dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApe_paterno(String ape_paterno) {
+        this.ape_paterno = ape_paterno;
+    }
+
+    public void setApe_materno(String ape_materno) {
+        this.ape_materno = ape_materno;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+}
---
+++ app/src/main/java/com/example/juicy/model/RptaGeneral.java
@@ -0,0 +1,20 @@
+package com.example.juicy.model;
+
+public class RptaGeneral {
+    private int code;
+    private String message;
+    private Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+}
