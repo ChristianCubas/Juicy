c6af02256cbd184fbe90469554ae13952655ac67
SebasNeco <71524442@usat.pe>
2025-10-24 19:18:00 -0500

    Metodos pago completo

---
+++ app/src/main/java/com/example/juicy/Model/MetodoPagoVentaRequest.java
@@ -0,0 +1,15 @@
+package com.example.juicy.Model;
+
+public class MetodoPagoVentaRequest {
+    private int id_cliente;
+    private int id_metodo_pago;
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public void setId_metodo_pago(int id_metodo_pago) {
+        this.id_metodo_pago = id_metodo_pago;
+    }
+
+}
