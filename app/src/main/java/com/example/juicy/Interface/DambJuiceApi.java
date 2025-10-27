9fde18df3f2cfca32cd94226bc29c9921a4f591f
cesargarcia19 <cesarandregg19@gmail.com>
2025-10-27 15:12:09 -0500

    actualización de rama

--- app/src/main/java/com/example/juicy/Interface/DambJuiceApi.java
+++ app/src/main/java/com/example/juicy/Interface/DambJuiceApi.java
@@ -2,11 +2,14 @@ package com.example.juicy.Interface;
 
 import com.example.juicy.model.AuthRequest;
 import com.example.juicy.model.AuthResponse;
+import com.example.juicy.model.MeResponse;
 import com.example.juicy.model.RegistrarClienteRequest;
 import com.example.juicy.model.RptaGeneral;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface DambJuiceApi {
@@ -15,4 +18,8 @@ public interface DambJuiceApi {
 
     @POST("api_registrar_cliente")
     Call<RptaGeneral> registrarCliente(@Body RegistrarClienteRequest request);
+
+    @GET("api_me")
+    Call<MeResponse> me(@Header("Authorization") String authHeader);
+
 }
